24884ad90f18d6ea697dff7e1bb2f8eb8bd4527c
Zach <zach.qin@chemistwarehouse.com.au>
2025-02-16 21:25:49 +1100

    Add pino for logging

--- apps/my-nestjs/src/app/cat/cat.module.ts
+++ apps/my-nestjs/src/app/cat/cat.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { CatService } from './cat.service';
 import { CatController } from './cat.controller';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport:
+          process.env.NODE_ENV !== 'production'
+            ? {
+                target: 'pino-pretty',
+                options: { colorize: true },
+              }
+            : undefined,
+      },
+    }),
+  ],
   controllers: [CatController],
   providers: [CatService],
   exports: [CatService],
--- apps/my-nestjs/src/app/cat/cat.service.ts
+++ apps/my-nestjs/src/app/cat/cat.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { Cat } from './cat.interface';
+import { PinoLogger } from 'nestjs-pino';
 
 @Injectable()
 export class CatService {
+  constructor(private readonly logger: PinoLogger) {}
+
   create(cat: Cat): string {
-    console.log(cat);
+    this.logger.info(cat, 'cat received');
     return 'This action adds a new cat';
   }
 }
